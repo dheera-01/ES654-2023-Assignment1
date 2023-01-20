cb0f2fb639bbe9e6e6a9042745f7942785a86920
dheera-01 <dheeraj.yadav@iitgn.ac.in>
2023-01-20 16:07:27 +0530

    Attributes

--- tree/base.py
+++ tree/base.py
@@ -16,16 +16,19 @@ from tree.utils import entropy, information_gain, gini_index
 
 np.random.seed(42)
 
-
 @dataclass
 class DecisionTree:
-    criterion: Literal["information_gain", "gini_index"]  # criterion won't be used for regression
+    # criterion: Literal["information_gain", "gini_index"]  # criterion won't be used for regression
     max_depth: int  # The maximum depth the tree can grow to
+
+    def get_attributes_X(self, X: pd.DataFrame) -> list:
+        return X.columns.tolist()
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
         """
         Function to train and construct the decision tree
         """
+
         pass
 
     def predict(self, X: pd.DataFrame) -> pd.Series:
@@ -47,3 +50,34 @@ class DecisionTree:
         Where Y => Yes and N => No
         """
         pass
+
+def test_decision_tree():
+    """
+    Function to test the decision tree
+    """
+    x = pd.DataFrame({
+        'Outlook': ['Sunny', 'Sunny', 'Overcast', 'Rain', 'Rain', 'Rain', 'Overcast', 'Sunny', 'Sunny', 'Rain', 'Sunny',
+                    'Overcast', 'Overcast', 'Rain'],
+        'Temperature': ['Hot', 'Hot', 'Hot', 'Mild', 'Cool', 'Cool', 'Cool', 'Mild', 'Cool', 'Mild', 'Mild', 'Mild',
+                        'Hot', 'Mild'],
+        'Humidity': ['High', 'High', 'High', 'High', 'Normal', 'Normal', 'Normal', 'High', 'Normal', 'Normal', 'Normal',
+                     'High', 'Normal', 'High'],
+        'Wind': ['Weak', 'Strong', 'Weak', 'Weak', 'Weak', 'Strong', 'Strong', 'Weak', 'Weak', 'Weak', 'Strong',
+                 'Strong', 'Weak', 'Strong'],
+        'height': [1,3,4,5,6,7,8,9,10,11,12,13,14,15]
+        })
+
+    y = pd.DataFrame({
+        'PlayTennis': ['No', 'No', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No']
+    })
+    # print(x)
+
+    index = x.groupby(["Outlook"]).groups['Overcast'].tolist()
+    print(index)
+    new_x = y.iloc[index]
+    print(new_x)
+
+    # print('entropy: ',entropy(y["PlayTennis"]))
+    # print('Gini: ', gini_index(y["PlayTennis"]))
+    tree1 = DecisionTree(max_depth=2)
+    # print(tree1.get_attributes_X(x));
--- tree/utils.py
+++ tree/utils.py
@@ -1,23 +1,35 @@
 import pandas as pd
-
+#manually imported
+import math
 
 def entropy(Y: pd.Series) -> float:
     """
     Function to calculate the entropy
     """
-
-    pass
-
+    n = len(Y)
+    rtr = 0.0
+    for i in Y.value_counts():
+        rtr = rtr - i/n * math.log2(i/n)
+    return rtr
 
 def gini_index(Y: pd.Series) -> float:
     """
     Function to calculate the gini index
     """
-    pass
+    n = len(Y)
+    rtr = 0.0
+    for i in Y.value_counts():
+        rtr = rtr + (i/n)*(1-i/n)
 
+    return rtr
 
 def information_gain(Y: pd.Series, attr: pd.Series) -> float:
     """
     Function to calculate the information gain
     """
-    pass
+    entropy_Y = entropy(Y)
+    # df_attr = pd.DataFrame({:Y, 'attr':attr})
+    index = attr.groupby(["Outlook"]).groups['Overcast'].tolist()
+    print(index)
+    new_x = y.iloc[index]
+    print(new_x)

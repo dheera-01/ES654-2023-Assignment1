d375bc2dbcfe7c8969721ad69eb2b58ba8098c09
Sandeep Desai <desai.sandeep@iitgn.ac.in>
2023-01-21 23:23:03 +0530

    implemented rmse, mae and accuracy

--- metrics.py
+++ metrics.py
@@ -1,6 +1,6 @@
 from typing import Union
 import pandas as pd
-
+import numpy as np
 
 def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
     """
@@ -14,13 +14,19 @@ def accuracy(y_hat: pd.Series, y: pd.Series) -> float:
     """
     assert y_hat.size == y.size
     # TODO: Write here
-    pass
+    ct=
+    total=len(y_hat)
+    for i in range(len(y_hat)):
+        if(y_hat[i]==y[i]):
+            ct=ct+1
+    return float(ct)/total
 
 
 def precision(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
     """
     Function to calculate the precision
     """
+    assert y_hat.size == y.size
     pass
 
 
@@ -28,6 +34,7 @@ def recall(y_hat: pd.Series, y: pd.Series, cls: Union[int, str]) -> float:
     """
     Function to calculate the recall
     """
+    assert y_hat.size==y.size
     pass
 
 
@@ -35,12 +42,15 @@ def rmse(y_hat: pd.Series, y: pd.Series) -> float:
     """
     Function to calculate the root-mean-squared-error(rmse)
     """
+    assert y_hat.size == y.size
+    return np.sqrt(np.power(y_hat-y,2).sum()/len(y))
 
-    pass
 
 
 def mae(y_hat: pd.Series, y: pd.Series) -> float:
     """
     Function to calculate the mean-absolute-error(mae)
     """
-    pass
+    assert y_hat.size == y.size
+    return abs(y_hat-y).sum()/len(y)
+

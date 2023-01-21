9cf6147f75e8087fb744db40983977099682d858
Sandeep Desai <desai.sandeep@iitgn.ac.in>
2023-01-21 20:31:32 +0530

    traversal and prediction implemented for discrete input case

--- tree/base.py
+++ tree/base.py
@@ -34,6 +34,13 @@ class DecisionTree:
     max_depth: int  # The maximum depth the tree can grow to
     root = Node('hello', True, {}, 'hello')
 
+
+    def traverse_discrete_input(self,node:Node,data:dict):
+        if(node.isLeaf==True):
+            return node.output
+        else:
+            return self.traverse_discrete_input(node.children[data[node.atrribute]],data)
+
     def get_attributes_X(self, X: pd.DataFrame) -> list:
         return X.columns.tolist()
 
@@ -216,13 +223,20 @@ class DecisionTree:
         # attributes=["outlook","humidity","rain"]
         attributes = self.get_attributes_X(X)
         # self.root = self.construct_tree(X,y,attributes,-1);
-        self.root=self.construct_tree_discrete_real(X,y,attributes,0)
+        self.root=self.construct_tree(X,y,attributes,0)
+
+
+    def predict_discrete_input(self,X:pd.DataFrame)->pd.Series:
+        y_pred = pd.Series()
+        for i in range(len(X)):
+            y_pred._set_value(i, self.traverse_discrete_input(self.root, X.iloc[i]))
+        return y_pred
 
     def predict(self, X: pd.DataFrame) -> pd.Series:
         """
         Funtion to run the decision tree on test inputs
         """
-        pass
+        return self.predict_discrete_input(X)
 
     def print_tree(self, node):
         if (node.isLeaf):
@@ -242,28 +256,30 @@ class DecisionTree:
         Where Y => Yes and N => No
         """
 
-# def test_decision_tree():
-#     """
-#     Function to test the decision tree
-#     """
-#     x = pd.DataFrame({
-#         'Outlook': ['Sunny', 'Sunny', 'Overcast', 'Rain', 'Rain', 'Rain', 'Overcast', 'Sunny', 'Sunny', 'Rain', 'Sunny',
-#                     'Overcast', 'Overcast', 'Rain'],
-#         'Temperature': ['Hot', 'Hot', 'Hot', 'Mild', 'Cool', 'Cool', 'Cool', 'Mild', 'Cool', 'Mild', 'Mild', 'Mild',
-#                         'Hot', 'Mild'],
-#         'Humidity': ['High', 'High', 'High', 'High', 'Normal', 'Normal', 'Normal', 'High', 'Normal', 'Normal', 'Normal',
-#                      'High', 'Normal', 'High'],
-#         'Wind': ['Weak', 'Strong', 'Weak', 'Weak', 'Weak', 'Strong', 'Strong', 'Weak', 'Weak', 'Weak', 'Strong',
-#                  'Strong', 'Weak', 'Strong'],
-#         })
-#
-#     y = pd.DataFrame({
-#         'PlayTennis': ['No', 'No', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No']
-#     })
-#
-#     tree1 = DecisionTree(max_depth=10)
-#     tree1.fit(x, pd.Series(y['PlayTennis']))
-#     print(tree1.root)
+def test_decision_tree():
+    """
+    Function to test the decision tree
+    """
+    x = pd.DataFrame({
+        'Outlook': ['Sunny', 'Sunny', 'Overcast', 'Rain', 'Rain', 'Rain', 'Overcast', 'Sunny', 'Sunny', 'Rain', 'Sunny',
+                    'Overcast', 'Overcast', 'Rain'],
+        'Temperature': ['Hot', 'Hot', 'Hot', 'Mild', 'Cool', 'Cool', 'Cool', 'Mild', 'Cool', 'Mild', 'Mild', 'Mild',
+                        'Hot', 'Mild'],
+        'Humidity': ['High', 'High', 'High', 'High', 'Normal', 'Normal', 'Normal', 'High', 'Normal', 'Normal', 'Normal',
+                     'High', 'Normal', 'High'],
+        'Wind': ['Weak', 'Strong', 'Weak', 'Weak', 'Weak', 'Strong', 'Strong', 'Weak', 'Weak', 'Weak', 'Strong',
+                 'Strong', 'Weak', 'Strong'],
+        })
+
+    y = pd.DataFrame({
+        'PlayTennis': ['No', 'No', 'Yes', 'Yes', 'Yes', 'No', 'Yes', 'No', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No']
+    })
+
+    tree1 = DecisionTree(max_depth=10)
+    tree1.fit(x, pd.Series(y['PlayTennis']))
+    X_pred=pd.DataFrame({"Outlook":['Sunny','Rain'],"Temperature":['Hot','Mild'],"Humidity":['High','Normal'],"Wind":['Strong','Weak']})
+    y_pred=tree1.predict(X_pred)
+    print(y_pred)
 
 # def test_decision_tree_real_discrete():
 #     x1 = np.random.uniform(0, 10, 100)
@@ -281,20 +297,21 @@ class DecisionTree:
 
 
 
-def test_decision_tree_discrete_real():
-    N = 30
-    P = 5
-    X = pd.DataFrame({i: pd.Series(np.random.randint(P, size=N), dtype="category") for i in range(5)})
-    y = pd.Series(np.random.randn(N))
-    column=["f1","f2","f3"]
-    lst=[[0,1,1,0,1],[1,0,1,0,0],[1,0,0,1,1]]
-    y=pd.Series([5.6,2.3,4.5,3.2,6.1])
-    X=pd.DataFrame({'f1':lst[0],'f2':lst[1],'f3':lst[2]})
-    tree1 = DecisionTree(max_depth=0)
-    print(X,y)
-    tree1.fit(X, y)
-    print(y.mean())
-    print()
-    print(tree1.root)
+# def test_decision_tree_discrete_real():
+#     N = 30
+#     P = 5
+#     X = pd.DataFrame({i: pd.Series(np.random.randint(P, size=N), dtype="category") for i in range(5)})
+#     y = pd.Series(np.random.randn(N))
+#     column=["f1","f2","f3"]
+#     lst=[[0,1,1,0,1],[1,0,1,0,0],[1,0,0,1,1]]
+#     y=pd.Series([5.6,2.3,4.5,3.2,6.1])
+#     X=pd.DataFrame({'f1':lst[0],'f2':lst[1],'f3':lst[2]})
+#     tree1 = DecisionTree(max_depth=0)
+#     tree1.fit(X, y)
+#     X_pred=pd.DataFrame({'f1':[1,0,0],'f2':[1,0,0],'f3':[1,0,1]})
+#     print("Mean of the data",y.mean())
+#     print(tree1.predict(X_pred))
 
-test_decision_tree_discrete_real()
+
+# test_decision_tree_discrete_real()
+test_decision_tree()
